daac94bbb547389b505fdd6422ddaf187944c003
NazifaSadia <nazifas323@gmail.com>
2020-05-14 15:24:50 +0600

    Changing CSS style dynamically

---
+++ 22. Changing CSS style dynamically/index.js
@@ -0,0 +1,9 @@
+var myPara = document.querySelector("#para");
+
+function addStyle() {
+    myPara.classList.add("para-style");
+}
+
+function removeStyle() {
+    myPara.classList.remove("para-style");
+}
